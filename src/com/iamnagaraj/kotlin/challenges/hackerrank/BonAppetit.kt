9066d853436238e17b46f3890bf7ba195a91d8b8
NaagAlgates <nagharaj.cse@gmail.com>
2019-09-08 11:05:49 +1000

    Bon Appetit

--- src/com/iamnagaraj/kotlin/challenges/hackerrank/BonAppetit.kt
+++ src/com/iamnagaraj/kotlin/challenges/hackerrank/BonAppetit.kt
@@ -1,23 +1,12 @@
 package com.iamnagaraj.kotlin.challenges.hackerrank
 
 fun main() {
-    val nk = readLine()!!.trimEnd().split(" ")
+    val k = readLine()!!.split(" ").last().toInt()
+    val mealCost = readLine()!!.split(" ").map { it.toInt() }
+    val charged = readLine()!!.toInt()
 
-    val n = nk[0].toInt()
-
-    val k = nk[1].toInt()
-
-    val bill = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
-
-    val b = readLine()!!.trim().toInt()
-
-    bonAppetit(bill, k, b)
-}
-
-fun bonAppetit(bill: Array<Int>, k: Int, b: Int): Unit {
-    var temp=0
-    bill.forEachIndexed { index, value ->
-        if(index != k)  temp += value
-    }
-    if((temp/2) == b) println("Bon Appetit") else println(b-(temp/2))
+    val actual = mealCost.filterIndexed { index, i ->
+        index!=k
+    }.sum()/2
+    println(if(charged == actual) "Bon Appetit" else charged-actual)
 }
